baaed792eb996fb156baa7379ad9f33565df7004
Maeesha <maeesha.rahman@gmail.com>
2020-09-25 13:31:44 -0400

    adding streamForm component and refactoring stream create and stream edit components

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    )
+// note: with react-router, each component needs to fetch its own data. can't rely on just one page fetching data if the user doesn't go to that page first (where data is fetched) then other pages (components) will have no data 
+
+class StreamEdit extends Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = formValues => {
+        this.props.editStream(this.props.match.params.id, formValues);
+    }
+
+    render() {
+        // console.log(this.props);
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm
+                    // initialValues is a special property name with redux-form
+                    // title and description are initial values that were passed to redux form (inside of the field name properties of the streamForm component)
+                    // the code below is the same as saying -> initialValues={{title: this.props.stream.title, description: this.props.stream.description}} // lodash _.pick method returns a new object that picks the title and description key-value pairs out (since we only care about the title and description for the initial values of the form)
+                    initialValues={_.pick(this.props.stream, 'title', 'description')}
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        )
+
+    }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    // console.log(ownProps);
+    // since this component is wrapped in route component in App.js, it has props object that contains route properties (like match, location and history)
+    // return stream that user is trying to edit 
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+
+class StreamForm extends Component {
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">
+                        {error}
+                    </div>
+                </div>
+            )
+        }
+    }
+    // Field component is hooked up to this function, anytime field component calls this function, it passes its props which we destructured to get all props for input elements 
+    renderInput = ({ input, label, meta }) => {
+        // console.log(meta)
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
+        // console.log(formProps);
+        // return <input onChange={formProps.input.onChange} value={formProps.input.value} />
+        // another syntax to add all the props to input element 
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <input {...input} />
+                {this.renderError(meta)}
+            </div>
+        )
+    }
+
+    onSubmit = formValues => {
+        this.props.onSubmit(formValues);
+    }
+
+    render() {
+        // redux form props -> console.log(this.props);
+        // handleSubmit is a cb function given by redux form
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field
+                    name="title"
+                    component={this.renderInput}
+                    label="Enter Title"
+                />
+                <Field
+                    name="description"
+                    component={this.renderInput}
+                    label="Enter Description"
+                />
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
+}
+
+const validate = formValues => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title'
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description'
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    form: 'streamForm',
+    validate
+})(StreamForm);
+
